24166a4a757ee19c03f7e7568da4cef30a74dfc1
Tobi-De <tobidegnon@protonmail.com>
2020-05-15 20:26:56 +0100

    my first commit

---
+++ tweetme/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tweetme.users.models import User
+from tweetme.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
